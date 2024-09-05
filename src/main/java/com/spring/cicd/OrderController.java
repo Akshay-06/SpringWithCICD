a7af894c214cfb29e1ca69e342b742f43eb7844d
Akshay-06 <akshayreddy1999@gmail.com>
2024-09-05 16:08:58 -0400

    First Commit

---
+++ src/main/java/com/spring/cicd/OrderController.java
@@ -0,0 +1,22 @@
+package com.spring.cicd;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    @Autowired
+    private OrderDao orderDao;
+
+    @GetMapping
+    public List<Order> fetchOrders() {
+        return orderDao.getOrders();
+    }
+
+}
---
+++ src/main/java/com/spring/cicd/SpringWithCicdApplication.java
@@ -0,0 +1,13 @@
+package com.spring.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithCicdApplication.class, args);
+	}
+
+}
